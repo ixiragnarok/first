b605785c9a3a449d7cdff8fdd70398fe4a0c29b6
Nikolas <ragnarokniki@gmail.com>
2024-06-18 21:27:50 +0300

    Adăugat fișierul afisare_numere.py

---
+++ afisare_numere.py
@@ -0,0 +1,6 @@
+# Cod simplu care afișează numerele de la 1 la 10
+
+# Folosim o buclă for pentru a parcurge numerele de la 1 la 10
+for numar in range(1, 11):
+    # Afișăm numărul curent
+    print(numar)
